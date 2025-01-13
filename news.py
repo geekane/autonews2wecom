a70e6dce2e378b0add064ce3ec46c82dca260dcb
geekane <32669380+geekane@users.noreply.github.com>
2025-01-13 17:36:04 +0800

    Update news.py

--- news.py
+++ news.py
@@ -10,13 +10,15 @@ import requests
 import json
 import argparse
 
-def fetch_hot_news(url, driver_path=None):
+
+def fetch_hot_news(url, driver_path=None, chromium_path=None):
     """
     使用 Selenium 获取动态渲染的页面 HTML (Edge 浏览器).
 
     Args:
         url (str): 目标 URL.
         driver_path (str, optional): WebDriver 的路径. 如果 WebDriver 在系统路径中则不需要提供.
+        chromium_path (str, optional): Chromium 可执行文件的路径.
 
     Returns:
         str: 页面 HTML 或 None (如果获取失败).
@@ -26,13 +28,22 @@ def fetch_hot_news(url, driver_path=None):
         chrome_options.add_argument("--headless")  # 无头模式
         chrome_options.add_argument("--no-sandbox")  # 禁用沙箱模式
         chrome_options.add_argument("--disable-dev-shm-usage")  # 禁用内存共享
+        if chromium_path:
+          chrome_options.binary_location = chromium_path
 
         if driver_path:
-            service = ChromeService(executable_path=driver_path)
+             service = ChromeService(executable_path=driver_path)
         else:
-            service = ChromeService()
+            # 尝试在当前目录下查找 chromedriver
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            driver_path = os.path.join(current_dir, 'chromedriver')
+            if os.path.exists(driver_path):
+               service = ChromeService(executable_path=driver_path)
+            else:
+                service = ChromeService()
 
         driver = webdriver.Chrome(service=service, options=chrome_options)
+
 
         driver.get(url)
 
@@ -70,6 +81,7 @@ def parse_html(html):
             hot_news.append({'title': title, 'link': link})  # 添加到热点列表
     return hot_news
 
+
 def send_to_wechat_bot(content):
     """
     发送消息到企业微信机器人
@@ -94,11 +106,12 @@ def send_to_wechat_bot(content):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--driver_path", help="Path to chromedriver")
+    parser.add_argument("--chromium_path", help="Path to chrome")
     args = parser.parse_args()
 
     url = 'https://rebang.today/tech?tab=ithome'
     # 请根据实际情况修改driver路径，如果driver在系统路径中，可以不传
-    html = fetch_hot_news(url, driver_path=args.driver_path)
+    html = fetch_hot_news(url, driver_path=args.driver_path, chromium_path = args.chromium_path)
     if html:
         news_list = parse_html(html)
         if news_list:
