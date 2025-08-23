e97099aeba7b50a78c4868521768f5ec638ea0c6
geekane <32669380+geekane@users.noreply.github.com>
2025-08-23 20:05:40 +0800

    Update chouyong_cli.py

--- chouyong_cli.py
+++ chouyong_cli.py
@@ -495,12 +495,6 @@ class CliRunner:
         logging.info("========== 开始执行步骤0: 同步 Life-Data.cn 数据 ==========")
         logging.info("==========================================================")
         
-        feishu_config = {
-            "app_id": "cli_a8ad5b52783b901c",
-            "app_secret": "DK8advnsYeChNF0yltKvKeqiQiYiAnyC",
-            "app_token": "MslRbdwPca7P6qsqbqgcvpBGnRh",
-            "table_id": "tbluVbrXLRUmfouv"
-        }
         cookie_file_for_life_data = '来客.json'
         target_url = "https://www.life-data.cn/store/my/chain/list?groupid=1768205901316096"
         download_dir = "downloads"
